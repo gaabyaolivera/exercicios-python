b77fa59b7d993f489a83353960c915a56b53e98c
Gabriella Oliveira <45859800+gaabyaolivera@users.noreply.github.com>
2020-08-06 14:30:48 -0300

    Add files via upload

---
+++ EstruturaSequencial/ex004.py
@@ -0,0 +1,6 @@
+n1 = float(input('1° nota: '))
+n2 = float(input('2° nota: '))
+n3 = float(input('3° nota: '))
+n4 = float(input('4° nota: '))
+media = (n1 + n2 + n3 + n4) / 4
+print('Média: {:.2f}'.format(media))
---
+++ EstruturaSequencial/ex005.py
@@ -0,0 +1,3 @@
+m = float(input('(M): '))
+conversao = m * 100
+print('{} metro(s) equivale a {} cm'.format(m, conversao))
